7bf37955ae2aa80b2b5cf943b15f2de9a7cedfcb
HikaruSato <hisato@solxyz.co.jp>
2021-04-03 17:32:30 +0900

    fix dependency and format

--- src/components/parser.ts
+++ src/components/parser.ts
@@ -4,30 +4,30 @@ import highlightjs from 'highlight.js'
 import cheerio from 'cheerio'
 
 marked.setOptions({
-    highlight: function(code, lang) {
-      return highlightjs.highlightAuto(code, [lang]).value;
+    highlight: function (code, lang) {
+        return highlightjs.highlightAuto(code, [lang]).value;
     },               // シンタックスハイライトに使用する関数の設定
     pedantic: false, // trueの場合はmarkdown.plに準拠する gfmを使用する場合はfalseで大丈夫
     gfm: true,       // GitHub Flavored Markdownを使用
     breaks: true,    // falseにすると改行入力は末尾の半角スペース2つになる
     sanitize: true,  // trueにすると特殊文字をエスケープする
     silent: false    // trueにするとパースに失敗してもExceptionを投げなくなる
-  });
-  
+});
+
 export default class MarkdownParser {
 
-    constructor(){}
+    constructor() { }
 
-    async parse(path: string): Promise<{html: string | null, err: any}>{
+    async parse(path: string): Promise<{ html: string | null, err: any }> {
 
-        const file =  fs.readFileSync(path);
-        if(!file) {
-            return {html: null, err: 'File Reading Error.'};
+        const file = fs.readFileSync(path);
+        if (!file) {
+            return { html: null, err: 'File Reading Error.' };
         }
 
         const parsed = marked.parse(file.toString())
 
-        return {html: parsed, err: null};
+        return { html: parsed, err: null };
     }
 
     async split(body: string, splitTag: string): Promise<string> {
@@ -35,7 +35,7 @@ export default class MarkdownParser {
         const $ = cheerio.load(body);
 
         $(splitTag).each((i: number, elem: any) => {
-            
+
         })
 
         return '<>';
